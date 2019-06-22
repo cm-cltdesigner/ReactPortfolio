717a6117284cc7609b77a4133bbaf0efe6c8d7bf
courtney manery <courtneymmanery@gmail.com>
2019-06-22 18:04:06 -0400

    configuring images

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,148 +1,150 @@
 let resumeData = {
-    "imagebaseurl":"http://www.courtneymanery.com",
-    "name": "Courtney Manery",
-    "role": "Graphic/ Web Developer",
-    "linkedinId":"https://www.linkedin.com/in/courtneymanery",
-    "roleDescription":" I am someone who strives for results yet enjoys the creative design process to get there efficiently. I am a very client-focused, reliable and detail-oriented developer who enjoys learning new tech.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/courtneymanery",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/cm-cltdesigner",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"facebook",
-          "url":"https://www.facebook.com/COURTNEYMANERYDESIGNS/",
-          "className":"fa fa-facebook"
-        },
-        {
-          "name":"twitter",
-          "url":"https://twitter.com/CourtneyManery",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"Graphic Design is my first love with an ceaseless passion for Front-End Web Development. I have extensive knowledge of modern Web techniques with random cravings for Hot Chocolate. Currently seeking a full-time opportunity with a talented and passionate group of developers/designers creating something awesome everyday. I believe in great leadership and teamwork, as well as being involved in an organization that believes in gaining a competitive edge and giving back to the community.",
-    "address":"Charlotte, NC USA",
-    "website":"courtneymanery.com",
-    "education":[
-      {
-        "UniversityName":"University of North Carolina",
-        "specialization":"Full Stack Web Development",
-        "MonthOfPassing":"June",
-        "YearOfPassing":"2019",
-        "Achievements":"Certification"
+  "imagebaseurl": "http://www.courtneymanery.com",
+  "name": "Courtney Manery",
+  "role": "Graphic/ Web Developer",
+  "linkedinId": "https://www.linkedin.com/in/courtneymanery",
+  "roleDescription": " I am someone who strives for results yet enjoys the creative design process to get there efficiently. I am a very client-focused, reliable and detail-oriented developer who enjoys learning new tech.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/courtneymanery",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/cm-cltdesigner",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "facebook",
+      "url": "https://www.facebook.com/COURTNEYMANERYDESIGNS/",
+      "className": "fa fa-facebook"
+    },
+    {
+      "name": "twitter",
+      "url": "https://twitter.com/CourtneyManery",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme": "Graphic Design is my first love with an ceaseless passion for Front-End Web Development. I have extensive knowledge of modern Web techniques with random cravings for Hot Chocolate. Currently seeking a full-time opportunity with a talented and passionate group of developers/designers creating something awesome everyday. I believe in great leadership and teamwork, as well as being involved in an organization that believes in gaining a competitive edge and giving back to the community.",
+  "address": "Charlotte, NC USA",
+  "website": "courtneymanery.com",
+  "education": [
+    {
+      "UniversityName": "University of North Carolina",
+      "specialization": "Full Stack Web Development",
+      "MonthOfPassing": "June",
+      "YearOfPassing": "2019",
+      "Achievements": "Certification"
+    },
+    {
+      "UniversityName": "Southern New Hampshire",
+      "specialization": "Marketing",
+      "MonthOfPassing": "Jan",
+      "YearOfPassing": "2019",
+      "Achievements": "Bachelors"
+    },
+    {
+      "UniversityName": "King's College",
+      "specialization": "Graphic Design",
+      "MonthOfPassing": "Dec",
+      "YearOfPassing": "2017",
+      "Achievements": "Associates"
+    },
+  ],
+  "work": [
+    {
+      "CompanyName": "GitCreativ LLC",
+      "specialization": " Freelance Graphic/ Web Developer",
+      "MonthOfLeaving": "Sept. 2016",
+      "YearOfLeaving": "- Present",
+      "Achievements": "Developing engaging and innovative Digital and Print Designs, Logo and Web Design, Company Branding Development, Social Media Management"
+    },
+    {
+      "CompanyName": "Allen Tate Companies",
+      "specialization": "Marketing Concierge",
+      "MonthOfLeaving": "Aug. 2018",
+      "YearOfLeaving": "- Mar. 2019",
+      "Achievements": "Social Media Management, Create Custom Digital Ads & Direct Mailers, Host Monthly Workshops To Enhance Real Estate Agents Marketing/Advertising, Develop Luxury Portfolio Properties Presentations For Real Estate Agents"
+    },
+    {
+      "CompanyName": "Tattoo Projects",
+      "specialization": "Graphic Design Intern",
+      "MonthOfLeaving": "Aug. 2017",
+      "YearOfLeaving": "- Nov. 2017",
+      "Achievements": " Developed New Design Concepts, Graphics, Page Layouts & Logos, Pitched Idea’s & Mockups for Advertising Campaigns, Ability To Interact, Communicate & Present Ideas to Design Team & Clients, Worked Independently & Closely With Senior Designers & Art Directors"
+    }
+  ],
+  "skillsDescription": "Languages",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Javascript"
+    },
+    {
+      "skillname": "React.js"
+    },
+    {
+      "skillname": "Adobe Creative Suite"
+    },
+  ],
+  "portfolio": [
+    {
+      "name": "MovieNow App",
+      "description": "mobileapp",
+      "imgurl": "https://i.ibb.co/ChWJ3px/modified-logo.png",
+      "url": "https://movie-now-app.herokuapp.com/",
+    },
+    {
+      "name": "Friend Finder App",
+      "description": "mobileapp",
+      "imgurl": "https://i.ibb.co/WWvtqWp/fff-app.jpg",
+      "url": "https://fascinating-friend-finder.herokuapp.com/",
       },
       {
-        "UniversityName":"Southern New Hampshire",
-        "specialization":"Marketing",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2019",
-        "Achievements":"Bachelors"
+        "name":"ClearMoon",
+        "description":"mobileapp",  
+        "imgurl":"https://webiconspng.com/wp-content/uploads/2017/09/Moon-PNG-Image-55224.png",
+        "url": "https://gitcreativ.com/ClearMoon/"
       },
       {
-        "UniversityName":"King's College",
-        "specialization":"Graphic Design",
-        "MonthOfPassing":"Dec",
-        "YearOfPassing":"2017",
-        "Achievements":"Associates"
-      },
-    ],
-    "work":[
-      {
-        "CompanyName":"GitCreativ LLC",
-        "specialization":" Freelance Graphic/ Web Developer",
-        "MonthOfLeaving":"Sept. 2016",
-        "YearOfLeaving":"- Present",
-        "Achievements":"Developing engaging and innovative Digital and Print Designs, Logo and Web Design, Company Branding Development, Social Media Management"
-      },
-      {
-        "CompanyName":"Allen Tate Companies",
-        "specialization":"Marketing Concierge",
-        "MonthOfLeaving":"Aug. 2018",
-        "YearOfLeaving":"- Mar. 2019",
-        "Achievements":"Social Media Management, Create Custom Digital Ads & Direct Mailers, Host Monthly Workshops To Enhance Real Estate Agents Marketing/Advertising, Develop Luxury Portfolio Properties Presentations For Real Estate Agents"
-      },
-      {
-        "CompanyName":"Tattoo Projects",
-        "specialization":"Graphic Design Intern",
-        "MonthOfLeaving":"Aug. 2017",
-        "YearOfLeaving":"- Nov. 2017",
-        "Achievements":" Developed New Design Concepts, Graphics, Page Layouts & Logos, Pitched Idea’s & Mockups for Advertising Campaigns, Ability To Interact, Communicate & Present Ideas to Design Team & Clients, Worked Independently & Closely With Senior Designers & Art Directors"
-      }
-    ],
-    "skillsDescription":"Languages",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Javascript"
-      },
-      {
-        "skillname":"React.js"
-      },
-      {
-        "skillname":"Adobe Creative Suite"
-      },
-    ],
-    "portfolio":[
-      {
-        "name":"MovieNow App",
+        "name":"Koscowix",
         "description":"mobileapp",
-        "imgurl":"https://i.ibb.co/ChWJ3px/modified-logo.png",
-        "url":"https://movie-now-app.herokuapp.com/",
+        "imgurl":"https://i.ibb.co/2kM54mZ/watch-icon-300x300.png",
+        "url": "https://koscowix-react-app.herokuapp.com/",
       },
-      {
-        "name":"Friend Finder App",
-        "description":"mobileapp",
-        "imgurl":"https://i.ibb.co/WWvtqWp/fff-app.jpg"
-      },
-      // {
-      //   // "name":"ClearMoon",
-      //   // "description":"mobileapp",  
-      //   // "imgurl":"https://i.ibb.co/vJcRjxG/Screen-Shot-2019-06-16-at-11-55-23-PM.png"
-      //   // https://cm-cltdesigner.github.io/ClearMoon/
-      // },
-      // {
-      //   // "name":"project4",
-      //   // "description":"mobileapp",
-      //   // "imgurl":"images/portfolio/phone.jpg"
-      // },
     ],
-    "testimonials":[
-      {
-        "description":"Outside of being a great designer, Courtney is a great person. She puts the people around her in a better mood and always goes beyond what is asked of her without hesitation. She is a highly conceptual thinker and puts together highly developed, thorough pieces. She would be a great fit on any creative team.",
-        "name":"Kenneth D."
-      },
-      {
-        "description":"Beyond being a very motivated and talented graphic design student, Courtney served as a role model for other students. She's polite, friendly and a pleasure to have in any professional setting.",
-        "name":"Ralph V."
-      },
-      {
-        "description":"Courtney Manery did an outstanding job on a marketing brochure for my business. She was creative and attentive to my needs regarding the overall objective for the piece. Any client can count on Courtney to provide fast service and strong desire to get the job done right!",
-        "name":"Teresa R."
-      },
-      {
-        "description":"Courtney made sure she gave me exactly what I wanted. She understood my vision and gave me a logo that reflects me.",
-        "name":"Jessica M."
-      },
-      {
-        "description":"She did an amazing job with my company website. She added design details that I wouldn't never thought of which generated business. You too should hire Courtney.",
-        "name":"Shannon M."
-      },
-      {
-        "description":"The work Mrs. Manery done for my company was amazing she made my vision come true with a little twist will hire her for my next project thanks...",
-        "name":"Dontè M."
-      },
-    ]
+"testimonials": [
+  {
+    "description": "Outside of being a great designer, Courtney is a great person. She puts the people around her in a better mood and always goes beyond what is asked of her without hesitation. She is a highly conceptual thinker and puts together highly developed, thorough pieces. She would be a great fit on any creative team.",
+    "name": "Kenneth D."
+  },
+  {
+    "description": "Beyond being a very motivated and talented graphic design student, Courtney served as a role model for other students. She's polite, friendly and a pleasure to have in any professional setting.",
+    "name": "Ralph V."
+  },
+  {
+    "description": "Courtney Manery did an outstanding job on a marketing brochure for my business. She was creative and attentive to my needs regarding the overall objective for the piece. Any client can count on Courtney to provide fast service and strong desire to get the job done right!",
+    "name": "Teresa R."
+  },
+  {
+    "description": "Courtney made sure she gave me exactly what I wanted. She understood my vision and gave me a logo that reflects me.",
+    "name": "Jessica M."
+  },
+  {
+    "description": "She did an amazing job with my company website. She added design details that I wouldn't never thought of which generated business. You too should hire Courtney.",
+    "name": "Shannon M."
+  },
+  {
+    "description": "The work Mrs. Manery done for my company was amazing she made my vision come true with a little twist will hire her for my next project thanks...",
+    "name": "Dontè M."
+  },
+]
   }
-  
-  export default resumeData
+
+export default resumeData
