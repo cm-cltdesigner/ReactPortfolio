81c046cbbf320b1492d90ed546abab44a4a10154
courtney manery <courtneymmanery@gmail.com>
2019-06-26 12:35:53 -0400

    commented out external links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -35,3 +35,21 @@ export default class Portfolio extends Component {
     );
   }
 }
+
+// export default class Home extends Component {
+//   render() {
+//       return (
+//           <Grid>
+//               <Jumbotron className="brainImg">
+//               </Jumbotron>
+//               <div class="footer"><center className="iconsBaby">
+//                   <Image src="giticon.png" className="githubIcon" to="https://github.com/Joeyryanbridges" />
+//                   <Image src="linkedinIcon.png" className="linkedinIcon" href="https://github.com/Joeyryanbridges" />
+//                   <Image src="SOFIcon.png" className="githubIcon" href="https://github.com/Joeyryanbridges" />
+//               </center>
+//               </div>
+//           </Grid>
+//       )
+  
+//   }
+// }
