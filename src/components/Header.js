783270b42af1927ee3eee0fae4854b3397c9ee7c
courtney manery <courtneymmanery@gmail.com>
2019-06-30 17:14:38 -0400

    clean up app.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ export default class Header extends Component {
             <ul id="nav" className="nav">
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                <li><a className="smoothscroll" href="#about">About</a></li>
-             <li><a className="smoothscroll" href="#resume">Resume</a></li>
+               <li><a className="smoothscroll" href="#resume">Resume</a></li>
                <li><a className="smoothscroll" href="#portfolio">Works</a></li>
                <li><a className="smoothscroll" href="#testimonials">Recommendations</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
@@ -33,8 +33,7 @@ export default class Header extends Component {
                                 <a href={item.url} target="_blank" rel="noopener noreferrer"><i className={item.className}> </i></a>
                               </li>
                             )
-                          }
-                    )
+                          })
                   }
                </ul>
             </div>
