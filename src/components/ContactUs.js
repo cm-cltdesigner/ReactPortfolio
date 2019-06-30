4f2c903c642e80f278b115acec49e5007adf70d8
courtney manery <courtneymmanery@gmail.com>
2019-06-30 16:58:31 -0400

    clean up contact.js

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -8,31 +8,25 @@ export default class ContactUs extends Component {
           <div className="ten columns">
             <p className="lead"></p>
             <div className="row">
-
               <div className="columns contact-details MakeWhite">
-
                 <h2>Contact Details</h2>
                 <p className="address">
-                  {/* <span>{resumeData.name}</span> */}
-
                   <span>
-                    {/* {resumeData.address} */}
+
                     980-598-7724<br />
                     cmanery4hire@gmail.com<br />
                     Based in Charlotte, NC<br />
                   </span>
-
-                  {/* <span>{resumeData.website}</span> */}
                 </p>
               </div>
             </div>
-        
-              <div class="section-heading">
-                <h3 class="title is-2">Hire Me !</h3>
-                <a href="https://drive.google.com/file/d/1rcrXKsOilhoUvaItVopCfSH_ms4Jw2wR/view?usp=sharing" class="button is-link is-medium" target="_blank" rel="noopener noreferrer">
-                  <span>Download My Resume</span>
-                </a>
-              </div>
+
+            <div class="section-heading">
+              <h3 class="title is-2">Hire Me !</h3>
+              <a href="https://drive.google.com/file/d/1rcrXKsOilhoUvaItVopCfSH_ms4Jw2wR/view?usp=sharing" class="button is-link is-medium" target="_blank" rel="noopener noreferrer">
+                <span>Download My Resume</span>
+              </a>
+            </div>
 
           </div>
         </div>
