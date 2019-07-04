53ac6cf338defec00d47a1e77b11fba6bf24c847
courtney manery <courtneymmanery@gmail.com>
2019-07-04 15:28:44 -0400

    test.js and init.js attempt

--- public/js/init.js
+++ public/js/init.js
@@ -96,20 +96,20 @@ jQuery(document).ready(function($) {
 /*	Modal Popup
 ------------------------------------------------------*/
 
-    $('.item-wrap a').magnificPopup({
+   //  $('.item-wrap a').magnificPopup({
 
-       type:'inline',
-       fixedContentPos: false,
-       removalDelay: 200,
-       showCloseBtn: false,
-       mainClass: 'mfp-fade'
+   //     type:'inline',
+   //     fixedContentPos: false,
+   //     removalDelay: 200,
+   //     showCloseBtn: false,
+   //     mainClass: 'mfp-fade'
 
-    });
+   //  });
 
-    $(document).on('click', '.popup-modal-dismiss', function (e) {
-    		e.preventDefault();
-    		$.magnificPopup.close();
-    });
+   //  $(document).on('click', '.popup-modal-dismiss', function (e) {
+   //  		e.preventDefault();
+   //  		$.magnificPopup.close();
+   //  });
 
 
 /*----------------------------------------------------*/
