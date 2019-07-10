7f32347c96d0c534eae4e76860859b139e65d63f
courtney manery <courtneymmanery@gmail.com>
2019-07-09 21:12:21 -0400

    changed image #2

--- src/resumeData.js
+++ src/resumeData.js
@@ -107,7 +107,7 @@ let resumeData = {
     {
       "name": "All News App",
       "description": "Design News app with a focus on Express and newly used NPM's, which allows the users to read the Latest News from 'theVerge'. I used minimal css design and express-handlebars, mongoose an NPM called cheerio and axios.",
-      "imgurl": "http://www.uww.edu/assets/images/urp/Generic-news-icon.png",
+      "imgurl": "https://www.trzcacak.rs/myfile/detail/27-278650_coming-soon-news-icon-png-orange.png",
       "url": "https://all-scrapped-app.herokuapp.com",
     },
     {
