2a09e00c07159928215ed78afa1a230fd95ac03a
courtney manery <courtneymmanery@gmail.com>
2019-07-12 16:08:40 -0400

    center sectino attempt

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -3,7 +3,7 @@ export default class ContactUs extends Component {
   render() {
     // let resumeData = this.props.resumeData;
     return (
-      <section id="contact" id="makeCenter">
+      <section id="contact">
         <div className="row section-head">
           <div className="ten columns">
             <p className="lead"></p>
@@ -12,12 +12,12 @@ export default class ContactUs extends Component {
                 <h2>Contact Details</h2>
                 <p className="address">
                   <span>
-
-                    980-598-7724<br />
-                    cmanery4hire@gmail.com<br />
-                    Based in Charlotte, NC<br />
-                    <img src="https://i.ibb.co/BP8JPVD/Logo-Sig-03.png" height="100px" width="100px" alt="sigImage"></img>
-                  </span>
+                    <div className="makeCenter">
+                      980-598-7724<br />
+                      cmanery4hire@gmail.com<br />
+                      Based in Charlotte, NC<br />
+                      <img src="https://i.ibb.co/BP8JPVD/Logo-Sig-03.png" height="100px" width="100px" alt="sigImage"></img>
+                    </div></span>
                 </p>
               </div>
             </div>
