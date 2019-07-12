aaa7d605be3bd35fc2923e073b2a19026251dce2
courtney manery <courtneymmanery@gmail.com>
2019-07-12 16:16:37 -0400

    center sectino attempt

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -3,7 +3,7 @@ export default class ContactUs extends Component {
   render() {
     // let resumeData = this.props.resumeData;
     return (
-      <section id="contact">
+      <center><section id="contact">
         <div className="row section-head make Center">
           <div className="ten columns">
             <p className="lead"></p>
@@ -30,7 +30,7 @@ export default class ContactUs extends Component {
             </div>
           </div>
         </div>
-      </section>
+      </section></center>
     );
   }
 }
